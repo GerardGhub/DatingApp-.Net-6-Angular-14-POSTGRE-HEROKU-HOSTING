97844064a4e673b8b0ae14838a773a58a41a3b99
GerardGhub <singian28@gmail.com>
2022-07-29 23:49:44 +0800

    Preparing the Angular app and serving this from API Server

--- API/Startup.cs
+++ API/Startup.cs
@@ -77,6 +77,9 @@ namespace API
             app.UseAuthentication();
             app.UseAuthorization();
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();

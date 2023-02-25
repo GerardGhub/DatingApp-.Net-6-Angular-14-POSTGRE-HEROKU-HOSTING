a15cf0fa60e45616d3cd8e6719e7bcae94d4b56e
GerardGhub <singian28@gmail.com>
2023-02-25 17:05:33 +0800

    updating to angular 13

---
+++ client/New folder/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
+import { PresenceService } from './_services/presence.service';
+// import { runInThisContext } from 'vm';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'Dating App';
+  users: any;
+  constructor(private accountService: AccountService, 
+    private presence: PresenceService) {
+
+  }
+
+  ngOnInit() {
+    this.setCurrentUser();
+  }
+
+
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    if (user) {
+      this.accountService.setCurrentUser(user);
+      this.presence.createHubConnection(user);
+    }
+  
+  }
+
+}
